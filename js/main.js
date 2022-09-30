d5b5b09bd3db209598e3aaa151fa1b9619a7ae23
Andrey <andrey_otin@mail.ru>
2022-09-30 17:06:48 +0300

    refactor: функция getRandomDecimal, убрал проверку на выходе

--- js/main.js
+++ js/main.js
@@ -9,26 +9,10 @@ const getRandomDecimal = (min, max , numberOfDecimals)=>{
   }
   const [tempMin , tempMax] = min > max ? swapTwoNumbers(min, max) : [min, max];
   const randomNumber = +((Math.random() * (tempMax - tempMin) + tempMin).toFixed(numberOfDecimals));
-  return tempMin <= randomNumber && randomNumber <= tempMax ? randomNumber : null ;
+  return randomNumber;
 };
 
-const getRandomNumber = (min, max)=>getRandomDecimal (min,max, 0);
+const getRandomNumber = (min, max)=>getRandomDecimal (min, max, 0);
 
 getRandomNumber(3,5);
 getRandomDecimal (0,4,3);
-
-//Число вылетает из диапозона бех проверки при следующих значениях:
-/*
-let random = 1;
-do {
-  random = getRandomDecimal(2.34, 4.67, 1);
-  console.log(random); // random 2.3 или 4.7
-} while (random !== null);
-
-let random = 1;
-do {
-  random = getRandomDecimal(6.2,6.7, 0);
-  console.log(random); // random 6 или 7
-} while (random !== null);
-
-*/
