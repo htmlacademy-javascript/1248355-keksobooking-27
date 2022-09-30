e7e5f8d7ce7a8d8c8076c6bb75b2db751e3aeb4d
Andrey <andrey_otin@mail.ru>
2022-09-30 13:19:58 +0300

    refactor: Сделан рефакторинг кода функций:getRandomNumber, getRandomDecimal, swapTwoNumbers

--- js/main.js
+++ js/main.js
@@ -1,37 +1,34 @@
 const checkNumbers = (...args) => args.every((arg)=>typeof arg === 'number' && arg >= 0);
 
-const swapTwoNumbers = (first, second) => {
-  const swapNumber = first;
-  first = second;
-  second = swapNumber;
-  return [first, second];
-};
-
-const getRandomNumber = (min, max)=>{
-  const isTargetNumbers = checkNumbers(min, max);
-  if(!isTargetNumbers){
-    return NaN;
-  }
-  if (min > max){
-    const numbers = swapTwoNumbers(min, max);
-    [min, max] = numbers;
-  }
-  const randomNumber = Math.round(Math.random() * (max - min) + min);
-  return min <= randomNumber && randomNumber <= max ? randomNumber : null ;
-};
+const swapTwoNumbers = (first, second) => [second, first];
 
 const getRandomDecimal = (min, max , numberOfDecimals)=>{
   const isTargetNumbers = checkNumbers(min, max, numberOfDecimals);
   if(!isTargetNumbers){
     return NaN;
   }
-  if (min > max){
-    const numbers = swapTwoNumbers(min, max);
-    [min, max] = numbers;
-  }
-  const randomNumber = +((Math.random() * (max - min) + min).toFixed(numberOfDecimals));
-  return min <= randomNumber && randomNumber <= max ? randomNumber : null ;
+  const [tempMin , tempMax] = min > max ? swapTwoNumbers(min, max) : [min, max];
+  const randomNumber = +((Math.random() * (tempMax - tempMin) + tempMin).toFixed(numberOfDecimals));
+  return tempMin <= randomNumber && randomNumber <= tempMax ? randomNumber : null ;
 };
 
-getRandomNumber(-5, 6.2);
-getRandomDecimal (2.2, 7, 5);
+const getRandomNumber = (min, max)=>getRandomDecimal (min,max, 0);
+
+getRandomNumber(3,5);
+getRandomDecimal (0,4,3);
+
+//Число вылетает из диапозона бех проверки при следующих значениях:
+/*
+let random = 1;
+do {
+  random = getRandomDecimal(2.34, 4.67, 1);
+  console.log(random); // random 2.3 или 4.7
+} while (random !== null);
+
+let random = 1;
+do {
+  random = getRandomDecimal(6.2,6.7, 0);
+  console.log(random); // random 6 или 7
+} while (random !== null);
+
+*/
