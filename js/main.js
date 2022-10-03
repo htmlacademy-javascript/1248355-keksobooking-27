588c1577c647b173795c0d8bfe83896cfb4ffd14
Andrey <andrey_otin@mail.ru>
2022-10-03 19:02:03 +0300

    refactor: перенес константы одной сущности в перечисления

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const LAT_MIN = 35.65;
-const LAT_MAX = 35.70;
-const LNG_MIN = 139.7;
-const LNG_MAX = 139.8;
-const MIN_PRICE = 1000;
-const MAX_PRICE = 100000;
-const MIN_GUESTS = 0;
-const MAX_GUESTS = 5;
-const MIN_ROOMS = 1;
-const MAX_ROOMS = 4;
+const Coordinates = {
+  LAT_MIN: 35.65,
+  LAT_MAX: 35.70,
+  LNG_MIN: 139.7,
+  LNG_MAX: 139.8
+};
+const Price = {
+  MIN: 1000,
+  MAX: 100000
+};
+const Guests = {
+  MIN: 0,
+  MAX: 5
+};
+const Rooms = {
+  MIN: 1,
+  MAX: 4
+};
 const NUMBER_OF_DECIMALS = 5;
 const NUMBER_OF_ADVERTISEMENTS = 10;
 const IMG_NUMBERS = Array.from({ length: NUMBER_OF_ADVERTISEMENTS }, (element, index) => index + 1);
@@ -60,8 +68,8 @@ const createRandomElementsArray = (elements, isRandomLength = true) => {
 const randomImgNumbers = createRandomElementsArray(IMG_NUMBERS, false);
 
 const createAdvertisement = (element, index) => {
-  const randomLat = getRandomDecimal(LAT_MIN, LAT_MAX, NUMBER_OF_DECIMALS);
-  const randomLng = getRandomDecimal(LNG_MIN, LNG_MAX, NUMBER_OF_DECIMALS);
+  const randomLat = getRandomDecimal(Coordinates.LAT_MIN, Coordinates.LAT_MAX, NUMBER_OF_DECIMALS);
+  const randomLng = getRandomDecimal(Coordinates.LNG_MIN, Coordinates.LNG_MAX, NUMBER_OF_DECIMALS);
   return {
     author: {
       avatar: `img/avatars/user${randomImgNumbers[index] < 10 ? (0).toString() + randomImgNumbers[index] : randomImgNumbers[index]}.png`
@@ -69,10 +77,10 @@ const createAdvertisement = (element, index) => {
     offer: {
       title: 'Лучший отель',
       address: `${randomLat}, ${randomLng}`,
-      price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+      price: getRandomNumber(Price.MIN, Price.MAX),
       type: getRandomArrayElement(TYPES),
-      rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
-      guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+      rooms: getRandomNumber(Rooms.MIN, Rooms.MAX),
+      guests: getRandomNumber(Guests.MIN, Guests.MAX),
       checkin: getRandomArrayElement(CHECKINS),
       checkout: getRandomArrayElement(CHECKOUTS),
       features: createRandomElementsArray(FEATURES),
